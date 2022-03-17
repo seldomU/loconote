9ca286f65e6523ca7f37613c925810fc7b3bf7fd
Uwe Koch <uwe.koch@code-it-studio.de>
2022-03-17 21:59:32 +0100

    code update

--- loconote/celltype-block-text.js
+++ loconote/celltype-block-text.js
@@ -2,7 +2,7 @@ import {createDomNode} from './util.js'
 import stdToolbox from './toolbox-stdlib-nocolor.js'
 
 let defaultConfig = {
-  "content": "<xml xmlns=\"https://developers.google.com/blockly/xml\"><block type=\"loco_textout_start\" id=\")w{WiwJ)J@WRz1aD0C77\" x=\"129\" y=\"59\" deletable=\"false\"><statement name=\"INSTRUCTIONS\"><block type=\"loco_textout_write\" id=\",AI5q_*VyiSiI1w6Nwgy\"><value name=\"VALUE\"><block type=\"text\" id=\"SRSA_d%[fKDtpQ]IFu$m\"><field name=\"TEXT\">hello world</field></block></value></block></statement></block></xml>",
+  "content": "<xml xmlns=\"https://developers.google.com/blockly/xml\"><block type=\"loco_textout_start\" id=\")w{WiwJ)J@WRz1aD0C77\" x=\"59\" y=\"59\" deletable=\"false\"><statement name=\"INSTRUCTIONS\"><block type=\"loco_textout_write\" id=\",AI5q_*VyiSiI1w6Nwgy\"><value name=\"VALUE\"><block type=\"text\" id=\"SRSA_d%[fKDtpQ]IFu$m\"><field name=\"TEXT\">hello world</field></block></value></block></statement></block></xml>",
   "blockly": {
     "toolboxfolders": stdToolbox
   }
@@ -74,7 +74,7 @@ Blockly.JavaScript.loco_textout_write = function(block) {
 
 function displaySource(cellConfig, id, parent){
 
-    let div = createDomNode(`<div id="${id}"> <img src="icons/codeblock.svg" width="30px" height="30px" alt="{}"> Computed text </div>`);
+    let div = createDomNode(`<div id="${id}"> </div>`);
     parent.prepend(div);
     return div;
 }
